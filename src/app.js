2b1aaf01fd6d5ff9537fcf395530fe341f4cec4b
Tomas Nocetti <nocetti.tomas@gmail.com>
2021-05-27 14:42:05 -0300

    Use path.join instead of '/'

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const json = require('express').json;
 const morgan = require('morgan');
 const cors = require('cors');
@@ -24,7 +25,7 @@ function start(){
 
     //Rutas
     startRoutes(app);
-    app.use('/static', express.static(__dirname + '/public'));
+    app.use('/static', express.static(path.join(__dirname, 'public')));
 
     app.use(notDefinedHandler);
     app.use(errorHandler);
