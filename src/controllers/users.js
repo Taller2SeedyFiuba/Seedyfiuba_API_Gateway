80e4f3d47efab6634dc0c06b1c8c44cd8b72ad85
Tomas Nocetti <nocetti.tomas@gmail.com>
2021-06-24 15:18:53 -0300

    feat(payments): create wallet on user-creation

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,6 +2,7 @@
 
 const axios = require('axios');
 const URL = process.env.USERS_MS;
+const PAYMENT_URL = process.env.PAYMENT_GTW_MS;
 
 exports.me = async(req, res, next) => {
   const reqRes = await axios.get(URL + '/users/' + req.id);
@@ -10,9 +11,13 @@ exports.me = async(req, res, next) => {
 
 exports.post = async(req, res, next) => {
   const reqRes = await axios.post(URL + '/users', {
-      id: req.id,
-      ... req.body
-    });
+    id: req.id,
+    ... req.body
+  });
+
+  await axios.post(PAYMENT_URL + '/wallets', {
+    ownerid: req.id
+  });
 
   res.status(201).json(reqRes.data);
 };
